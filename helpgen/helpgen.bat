4c59ab5771872403aa8335df357a1b1e45150566
Alexandr Promakh <s-promakh@ya.ru>
2012-10-14 12:35:15 +0600

    add .ds_store to gitignore and delete fucking file

--- helpgen/helpgen.bat
+++
@@ -1,8 +0,0 @@
-@echo off 
-chcp 65001
-if "%1"=="/?" (
-	echo This command display the help for all commands in the windows console to file helphen.txt
-	goto :eof
-)
-echo/>helpgen.txt
-for /f %%i in ('help ^| findstr /r /c:"^[A-Z]"') do help %%i 2>nul 1>>helpgen.txt
