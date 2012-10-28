289e57768c9c11f6814ce1c668d2de7845ee73aa
Promakh Alexandr <s-promakh@ya.ru>
2012-10-28 20:39:30 +0600

    finish int/ext

--- int_ext/promakh/int.bat
+++ int_ext/promakh/int.bat
@@ -6,14 +6,18 @@ if "%1"=="/?" (
 )
 
 for /f %%k in ('help ^| findstr /r /c:"^[A-Z]"') do (
-	for %%i in (%PATHEXT%) do (
+	set external=0
+	for %%i in (%PATHEXT%) do (	
 		for %%j in (%Path%) do (
 			set dr=%%j\
 			set dr=!dr:\\=\!
-			if not exist !dr!%%k%%i (
-				echo %%k - internal commands
+			if exist !dr!%%k%%i (
+				set external=1
 			)
 		)
+	)
+	if not !external!==1 (
+		echo %%k - internal command
 	)
 )
 endlocal
